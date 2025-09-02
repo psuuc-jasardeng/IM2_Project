e398e2dfe2c560d5e0a3d73deee0f7d433c1864f
psuuc-jasardeng <jereimiahsardeng@gmail.com>
2025-09-02 21:20:49 +0800

    HW1

---
+++ HW1/HW1_Sardeng/HW1_Sardeng/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        while (true)
+        {
+            Console.Write("Enter string: ");
+            string word = Console.ReadLine();
+
+            if (word == "*")
+                break;
+
+            string original = word;
+            string lowercaseOriginal = word.ToLower();
+            string reversed = ReverseString(lowercaseOriginal);
+            bool isPalindrome = IsPalindrome(lowercaseOriginal, reversed);
+
+            Console.WriteLine("Original String\t\tReversed String\t\tPalindrome?");
+            Console.WriteLine($"{original,-15}\t\t{reversed,-15}\t\t{(isPalindrome ? "Yes" : "No")}");
+        }
+        Console.WriteLine("\n//End execution");
+        Console.ReadKey();
+    }
+
+    static string ReverseString(string str)
+    {
+        char[] charArray = str.ToCharArray();
+        int left = 0;
+        int right = charArray.Length - 1;
+
+        while (left < right)
+        {
+            char temp = charArray[left];
+            charArray[left] = charArray[right];
+            charArray[right] = temp;
+            left++;
+            right--;
+        }
+        return new string(charArray);
+    }
+
+    static bool IsPalindrome(string original, string reversed)
+    {
+        return original == reversed;
+    }
+}
